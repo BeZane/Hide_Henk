5ddce42b3ead8d82ef2f3fdbfff39100878bbdc3
TomVerschueren <tomversch@msn.Com>
2016-11-24 15:17:05 +0100

    GamePanel onderste deel.

--- app/src/main/java/entertaiment/shurmans/jarno/tomverschueren/hide_henk/GamePanel.java
+++ app/src/main/java/entertaiment/shurmans/jarno/tomverschueren/hide_henk/GamePanel.java
@@ -1,10 +1,33 @@
 package entertaiment.shurmans.jarno.tomverschueren.hide_henk;
 
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
  * Created by TomVerschueren on 24/11/2016.
  */
 
-public class GamePanel {
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
-    //TESTING
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
 }
